87f3a32c55abf7aaadc9cfecdaaa57040298f333
forestcorgii <seanivanf@gmail.com>
2020-05-05 00:12:58 +0800

    added design and events

--- Titanium-Data-Sender/Form1.Designer.vb
+++ Titanium-Data-Sender/Form1.Designer.vb
@@ -22,17 +22,67 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.lstLogs = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.tbPath = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
+        '
+        'lstLogs
+        '
+        Me.lstLogs.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lstLogs.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3})
+        Me.lstLogs.Location = New System.Drawing.Point(12, 38)
+        Me.lstLogs.Name = "lstLogs"
+        Me.lstLogs.Size = New System.Drawing.Size(528, 252)
+        Me.lstLogs.TabIndex = 0
+        Me.lstLogs.UseCompatibleStateImageBehavior = False
+        Me.lstLogs.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "ID"
+        Me.ColumnHeader1.Width = 59
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Site"
+        Me.ColumnHeader2.Width = 81
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Date Added"
+        Me.ColumnHeader3.Width = 103
+        '
+        'tbPath
+        '
+        Me.tbPath.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tbPath.Location = New System.Drawing.Point(12, 12)
+        Me.tbPath.Name = "tbPath"
+        Me.tbPath.Size = New System.Drawing.Size(528, 20)
+        Me.tbPath.TabIndex = 1
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(463, 167)
+        Me.ClientSize = New System.Drawing.Size(552, 302)
+        Me.Controls.Add(Me.tbPath)
+        Me.Controls.Add(Me.lstLogs)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Titanium Data Sender"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents lstLogs As ListView
+    Friend WithEvents tbPath As TextBox
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents ColumnHeader3 As ColumnHeader
 End Class
--- Titanium-Data-Sender/Form1.vb
+++ Titanium-Data-Sender/Form1.vb
@@ -1,3 +1,30 @@
 ﻿Public Class Form1
+    Private formName As String
+
+    Private Sub Form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        formName = Application.ProductName & " v" & Application.ProductVersion
+        Me.Text = formName
+
+
+
+    End Sub
+
+
+
+    Private Sub tbPath_TextChanged(sender As Object, e As EventArgs) Handles tbPath.TextChanged
+
+    End Sub
+
+    Private Sub tbPath_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles tbPath.MouseDoubleClick
+
+    End Sub
+
+
+
+
+    Private Sub changeStatus(message As String)
+        Me.Text = formName & " " & message
+    End Sub
+
 
 End Class
