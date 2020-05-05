e7589aedb0bc5843b70a3abf487f3a8f14d202a0
forestcorgii <seanivanf@gmail.com>
2020-05-05 18:21:09 +0800

    added 'readData' function for tracking new titanium data logs.

--- Titanium-Data-Sender/Form1.vb
+++ Titanium-Data-Sender/Form1.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.Net
 Imports System.IO
 Imports System.Text
+Imports System.Data.OleDb
 
 Imports Newtonsoft.Json
 
@@ -30,6 +31,37 @@ Public Class Form1
         End If
         changeStatus("Running" & dots)
     End Sub
+
+
+
+
+    Private lastRecord As String
+    Private Sub readData()
+        Dim con As New OleDbConnection(tbPath.Text)
+        Dim com As New OleDb.OleDbCommand("select * from Emp_InOut order by `date`, `time` desc", con)
+        Dim rdr As OleDb.OleDbDataReader = com.ExecuteReader()
+
+        Dim previousRecord As String = lastRecord
+        While rdr.Read
+            Dim _date As String = rdr.Item("date")
+            Dim _time As String = rdr.Item("time")
+            Dim logDatetime As String = String.Format("20{0}-{1}-{2} {3}:{4}", _date.Substring(0, 2),
+                                                      _date.Substring(2, 2), _date.Substring(4, 2),
+                                                       _time.Substring(0, 2), _time.Substring(2, 2))
+
+            Dim id As String = rdr.Item("id")
+            Dim record As String = String.Format("{0} {1}", id, logDatetime)
+
+            If Not lastRecord = record Then
+                'send
+                previousRecord = record
+            Else   'end
+            End If
+        End While
+
+        lastRecord = previousRecord
+    End Sub
+
 
     Private Function sendTimeLog(id As String, site As String, date_added As Date) As Boolean
         Dim responseFromServer As String = ""
@@ -77,6 +109,7 @@ Public Class Form1
 
         Return responseFromServer
     End Function
+
 
     Private Sub changeStatus(message As String)
         Me.Text = formName & " " & message
